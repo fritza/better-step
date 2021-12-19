15a19dc41eb20d8934f82b6f785054b969d5d5f5
Fritz Anderson <fritza@uchicago.edu>
2021-12-19 11:43:39 -0600

    Initial Commit

---
+++ Better Step/Better_StepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_StepApp.swift
+//  Better Step
+//
+//  Created by Fritz Anderson on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Better_StepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
