12094a291758cc551a6f5d70509f62310d8be97a
Fritz Anderson <fritza@uchicago.edu>
2021-12-19 13:18:13 -0600

    Refactor SurveyView's generic content into GenericInstructionView
    Membership of GenericInstructionView and SurveyView in working copy were undefined ("?") ?!

---
+++ Better Step/GenericInstructionView.swift
@@ -0,0 +1,72 @@
+//
+//  GenericInstructionView.swift
+//  Better Step
+//
+//  Created by Fritz Anderson on 12/19/21.
+//
+
+import SwiftUI
+
+struct GenericInstructionView: View {
+    private let imageScale: CGFloat = 0.6
+
+    let titleText: String
+    let bodyText: String
+    let sfBadgeName: String
+    var proceedTitle: String
+
+    // And then we'll have to decide how to handle the "proceed" action
+    let proceedClosure: () -> Void
+
+    init(titleText: String,
+         bodyText: String,
+         sfBadgeName: String,
+         proceedTitle: String = "Proceed",
+         proceedClosure: @escaping () -> Void) {
+       ( self.titleText, self.bodyText, self.sfBadgeName, self.proceedTitle, self.proceedTitle) =
+        ( titleText, bodyText, sfBadgeName, proceedTitle, proceedTitle)
+        self.proceedClosure = proceedClosure
+    }
+
+    var body: some View {
+        GeometryReader {
+            proxy in
+            HStack {
+                Spacer()
+                VStack {
+                    Text(titleText)
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                    Spacer()
+                    Image(systemName: sfBadgeName)
+                        .resizable()
+                        .foregroundColor(.accentColor)
+                        .frame(
+                            width: proxy.size.width * imageScale,
+                            height: proxy.size.width * imageScale, alignment: .center)
+                    Spacer()
+                    Text(bodyText)
+                        .font(.body)
+                    Spacer()
+
+                    Button(proceedTitle) {
+                        proceedClosure()
+                    }
+                }
+                Spacer()
+            }
+        }
+//        .padding()
+    }
+}
+
+struct GenericInstructionView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenericInstructionView(titleText: "Survey",
+                               bodyText: "Just do what we tell you and nobody gets hurt.",
+                               sfBadgeName: "trash.slash",
+                               proceedTitle: "Go!") {
+            // do something
+        }
+    }
+}
--- Better Step/SurveyView.swift
+++ Better Step/SurveyView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-let narrativeString = """
+let surveyNarrativeString = """
 This exercise will assess your stride and pace though a short (six-minute) walk. An alarm sound to signal the beginning and the end of the exercise.
 
 Tap “Proceed" when you are ready
@@ -18,48 +18,12 @@ struct SurveyView: View {
     @State private  var isProceeding = false
 
     var body: some View {
-        GeometryReader {
-            proxy in
-            HStack {
-                Spacer()
-                VStack() {
-                    Text("DASI Survey")
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                    Spacer()
-                    Image(systemName: "checkmark.square")
-                    // .renderingMode(.template)
-                    // why not renderingMode?
-                        .resizable()
-                        .foregroundColor(.accentColor)
-                        .frame(
-                            width: proxy.size.width * imageScale,
-                            height: proxy.size.width * imageScale, alignment: .center)
-                    Spacer()
-                    Text(narrativeString)
-                        .font(.body)
-                    Spacer()
+        GenericInstructionView(titleText: "DASI Survey",
+                               bodyText: surveyNarrativeString, sfBadgeName: "checkmark.square",
+        proceedTitle: "Go ahead on") {
 
-                    if isProceeding {
-                        Text("PROCEEDING!")
-                    }
-                    else {
-                        Button("Proceed") {
-                            isProceeding.toggle()
-                            // navigate to the in-walk view
-                        }
-                    }
-                }
-                Spacer()
-            }
-        }.padding()
-            /*
-             VStack(alignment: .center) {
-             Text("DASI Survey")
-             Image(systemName: "figure.walking")
-             .frame(width: proxy.size.width*0.8, height: proxy.size.width*0.8, alignment: .center)
-             }
-             */
+        }
+        .padding(32)
     }
 }
 
