b1bce7e265b7a87098eea0dafbb25b8c6c4c7e73
Fritz Anderson <fritza@uchicago.edu>
2022-01-06 16:14:04 -0600

    EmailFormView uses GeometryReader to allocate space for the label.

--- Better Step/Views/EmailFormView.swift
+++ Better Step/Views/EmailFormView.swift
@@ -13,36 +13,31 @@ struct EmailFormView: View {
 //    @Binding var addressContent: EmailClient
     @EnvironmentObject var config: Configurations
     let title: String
-    let offset: CGFloat
     let keyboardType: UIKeyboardType
 
-    init(title: String,
-         fieldOffset: CGFloat = 64,
-         keyboard: UIKeyboardType = .emailAddress) {
+    init(title: String, keyboard: UIKeyboardType = .emailAddress) {
         self.title = title
-        self.offset = fieldOffset
         self.keyboardType = keyboard
     }
 
     var body: some View {
-        HStack {
-            Text(title)
-                .frame(width: offset)
-            TextField("IGNORED 1",
-                      text: $config.emailAddress,
-                      prompt: Text("reporting address"))
-                .keyboardType(.emailAddress)
-            Button(action: { config.emailAddress = "" }) {
-                Image(systemName: "multiply.circle.fill")
-                    .renderingMode(.template)
-                    .foregroundColor(.gray)
-                    .opacity(0.5)
-            }
+        GeometryReader { proxy in
+            HStack {
+                Text(title)
+                    .frame(width: proxy.size.width * 0.25,
+                           alignment: .leading)
+                TextField("IGNORED 1",
+                          text: $config.emailAddress,
+                          prompt: Text("reporting address"))
+                    .keyboardType(.emailAddress)
+                Button(action: { config.emailAddress = "" }) {
+                    Image(systemName: "multiply.circle.fill")
+                        .renderingMode(.template)
+                        .foregroundColor(.gray)
+                        .opacity(0.5)
+                }
                 .padding([.trailing], 0)
-//                .opacity(amSelected ? 0.5 : 0.0)
-                // Shift the clear button to align with the
-                // trailing edge of the stepper.
-//            }
+            }
         }
     }
 }
@@ -54,9 +49,7 @@ struct EmailFormView_Previews: PreviewProvider {
 
     static var previews: some View {
         VStack {
-            EmailFormView(title: "Report:"
-//                         , fieldOffset: 72
-            )
+            EmailFormView(title: "Report:")
                 .frame(width: 360, height: 60)
                 .environmentObject(config)
         }
