11c884cf60b8bd50124e6cf0e86e00b03387339d
Fritz Anderson <fritza@uchicago.edu>
2022-01-11 14:28:58 -0600

    "Proceed" button for SurveyView (intro) now links into the first DASIQuestionView.

--- Better Step/Views/SurveyView.swift
+++ Better Step/Views/SurveyView.swift
@@ -12,18 +12,28 @@ This exercise asks you to respond to questions from a standard assessment of how
 """
 
 struct SurveyView: View {
+    func callToActionLink(buttonTitle: String,
+                          linkLabel: String) -> some View
+    {
+        NavigationLink(destination: {
+            DASIQuestionView(question: DASIQuestion.with(id: 1))
+        }) {
+            Text(buttonTitle)
+        }
+    }
+
     var body: some View {
         NavigationView {
             VStack {
+                Spacer()
                 GenericInstructionView(
                     bodyText: surveyNarrative, sfBadgeName: "checkmark.square")
                     .navigationTitle("DASI Survey")
                     .padding(32)
-                Button("Proceed!") {
-                    NavigationLink("Proceed", destination: {
-                        DASIQuestionView(question: DASIQuestion.with(id: 1))
-                    })
-                }
+
+                callToActionLink(buttonTitle: "Proceed",
+                                   linkLabel: "Proceed to Survey")
+                Spacer()
             }
         }
     }
