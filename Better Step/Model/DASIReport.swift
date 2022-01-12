bc4462343ff01ae781fada1b26c8f876c212cea0
Fritz Anderson <fritza@uchicago.edu>
2022-01-12 14:28:23 -0600

    Split DASIReportDocument into its own file.
    I'd missed that [Reference]FileDocument were protocols.

--- Better Step/Model/DASIReport.swift
+++ Better Step/Model/DASIReport.swift
@@ -17,64 +17,6 @@ import CodableCSV
  But I need to access the document based on the user ID.
  */
 
-// MARK: - DASIReportDocument
-final class DASIReportDocument: ReferenceFileDocument, ObservableObject
-//, Codable
-{
-    typealias Snapshot = DASIReport
-
-
-
-    enum Errors: Error {
-        case wrongDataType(UTType)
-        case notRegularFile
-        case noReadableReport
-    }
-
-    static var readableContentTypes: [UTType] = [.commaSeparatedText]
-    @Published var report: DASIReport
-
-    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        let data = try JSONEncoder().encode(report)
-        let retval: FileWrapper =  FileWrapper(regularFileWithContents: data)
-        return retval
-    }
-
-    func fileWrapper(snapshot: Snapshot, configuration: WriteConfiguration) throws -> FileWrapper {
-        return try fileWrapper(configuration: configuration)
-    }
-
-    func snapshot(contentType: UTType) throws -> Snapshot {
-        return report
-    }
-
-    init() {
-        report = DASIReport(forSubject: "FOR RENT")
-    }
-
-//    snap
-
-    init(configuration: FileDocumentReadConfiguration) throws {
-        // Accept content type
-        let (fileType, wrapper) = (configuration.contentType, configuration.file)
-        guard Self.readableContentTypes.contains(fileType) else {
-            throw Errors.wrongDataType(fileType)
-        }
-        guard wrapper.isRegularFile else {
-            report = DASIReport()
-            return
-        }
-
-        guard let data = wrapper.regularFileContents else {
-            throw Errors.noReadableReport
-        }
-
-        let retval = try JSONDecoder().decode(DASIReport.self,
-                                              from: data)
-        report = retval
-    }
-}
-
 // A DASI report is an array of all DASI responses, verify it'e all IDs, all consecutive.
 // MARK: - DASIReport
 final class DASIReport: ObservableObject, Codable {
---
+++ Better Step/Model/DASIReportDocument.swift
@@ -0,0 +1,70 @@
+//
+//  DASIReportDocument.swift
+//  Better Step
+//
+//  Created by Fritz Anderson on 1/12/22.
+//
+
+import Foundation
+import SwiftUI
+import UniformTypeIdentifiers
+import CodableCSV
+
+// MARK: - DASIReportDocument
+final class DASIReportDocument: ReferenceFileDocument, ObservableObject
+//, Codable
+{
+    typealias Snapshot = DASIReport
+
+
+
+    enum Errors: Error {
+        case wrongDataType(UTType)
+        case notRegularFile
+        case noReadableReport
+    }
+
+    static var readableContentTypes: [UTType] = [.commaSeparatedText]
+    @Published var report: DASIReport
+
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        let data = try JSONEncoder().encode(report)
+        let retval: FileWrapper =  FileWrapper(regularFileWithContents: data)
+        return retval
+    }
+
+    func fileWrapper(snapshot: Snapshot, configuration: WriteConfiguration) throws -> FileWrapper {
+        return try fileWrapper(configuration: configuration)
+    }
+
+    func snapshot(contentType: UTType) throws -> Snapshot {
+        return report
+    }
+
+    init() {
+        report = DASIReport(forSubject: "FOR RENT")
+    }
+
+//    snap
+
+    init(configuration: FileDocumentReadConfiguration) throws {
+        // Accept content type
+        let (fileType, wrapper) = (configuration.contentType, configuration.file)
+        guard Self.readableContentTypes.contains(fileType) else {
+            throw Errors.wrongDataType(fileType)
+        }
+        guard wrapper.isRegularFile else {
+            report = DASIReport()
+            return
+        }
+
+        guard let data = wrapper.regularFileContents else {
+            throw Errors.noReadableReport
+        }
+
+        let retval = try JSONDecoder().decode(DASIReport.self,
+                                              from: data)
+        report = retval
+    }
+}
+
