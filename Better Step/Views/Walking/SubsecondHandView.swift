f31ffd2b745659d2e0440c871598a616d920c583
Fritz Anderson <fritza@uchicago.edu>
2022-01-21 15:06:48 -0600

    Move SubsecondHandView's contents so it's centered on its frame.

--- Better Step/Views/Walking/SubsecondHandView.swift
+++ Better Step/Views/Walking/SubsecondHandView.swift
@@ -10,29 +10,36 @@ import CoreGraphics
 
 struct SubsecondHandView: View {
     @State var normalizedAngle: CGFloat
-    @State var sweepTimer: Timer?
+    @EnvironmentObject var timer: WrappedTimer
+
+    func midpoint(within proxy: GeometryProxy) -> CGPoint {
+        let middle = proxy.size.short / 2.0
+        return CGPoint(x: middle, y: middle)
+    }
+
     var body: some View {
         GeometryReader { proxy in
             Rectangle()
                 .rotation(
-                    Angle(degrees: 180.0 + (normalizedAngle * 360.0)),
+                    Angle(degrees: 180.0 + (timer.fractionalSeconds * 360.0)),
                           anchor: UnitPoint(x: 0.5, y: 0.05))
-                .frame(width: 2.0, height: proxy.size.short, alignment: .center)
-        }
-        
-        .onAppear {
-            sweepTimer = Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { t in
-                normalizedAngle += 0.01
-            }
+                .offset(midpoint(within: proxy))
+                .frame(width: 2.0, height: proxy.size.short/2.0, alignment: .center)
         }
     }
 }
 
 struct SubsecondHandView_Previews: PreviewProvider {
+    static func previewWT() -> WrappedTimer {
+        let retval = WrappedTimer(5.0)
+        return retval
+    }
+
     static var previews: some View {
         SubsecondHandView(normalizedAngle: 0.4)
             .foregroundColor(.red)
             .frame(width: 100, height: 100, alignment: .center)
             .border(.green, width: 0.5)
+            .environmentObject(previewWT())
     }
 }
