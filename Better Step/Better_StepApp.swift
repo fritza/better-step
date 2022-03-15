68767ee0937597381e489a2af9706e7242027669
Fritz Anderson <fritza@uchicago.edu>
2022-03-15 12:26:41 -0500

    Remove time interval from initializer for MotionManager; remove non-sequence startAccelerometry()
    Regretfully.

--- Better Step/Better_StepApp.swift
+++ Better Step/Better_StepApp.swift
@@ -10,10 +10,19 @@ import SwiftUI
 
 // MARK: - @AppStorage
 enum AppStorageKeys: String {
+    /// How long the timed walk is to last, in _minutes,_ e.g. 6.
     case walkInMinutes
+    /// The frequency in Hertz (e.g. 120) for sampling the accelerometer.
+    case walkSamplingRate
+    /// If `false`, report acceleration in three axes; otherwise as the vector magnitude.
     case reportAsMagnitude
+    /// The email address to receive report archive files.
     case reportingEmail
-
+    /// Whether to include the timed walk
+    case includeWalk
+    /// Whether to include the DASI survey
+    case includeSurvey
+    /// The last known subject ID.
     case subjectID      // Is the right place?
     // We'd rather set it each time, right?
     // FIXME: Clear subjectID when transmitted
@@ -31,6 +40,7 @@ enum AppStorageKeys: String {
 @main
 struct Better_StepApp: App {
 
+    // FIXME: App does not interpose the app onboard sheet.
     @AppStorage(AppStorageKeys.subjectID.rawValue) var subjectID = ""
     let globals = GlobalState()
 
