465e839773af0381cc8c5dc29af7b03c47286995
Fritz Anderson <fritza@uchicago.edu>
2022-03-15 12:35:45 -0500

    SetupView: Select whether to offer DASI and Walk phases.
    Add a SetupView subview for clearing data series (WASN'T THIS THE PREVIOUS COMMIT?)
    
    SetupView adds @AppStorage properties for
    Configurations observable no longer publishes email address or walk duration.

--- Better Step/Views/SetupView.swift
+++ Better Step/Views/SetupView.swift
@@ -11,8 +11,6 @@ import SwiftUI
 final class Configurations: ObservableObject, CustomStringConvertible {
     var emailAddress: String
     var durationInMinutes: Int
-//    @Published var emailAddress: String
-//    @Published var durationInMinutes: Int
 
     var description: String {
         let emailString = emailAddress
@@ -29,7 +27,48 @@ final class Configurations: ObservableObject, CustomStringConvertible {
 private enum ControlFocus: String {
     case emailField
     case durationStepper
+}
 
+struct ClearingView: View {
+    @State private var lastID: Int = 0
+
+   let clearingTitles: [(String, Bool, Int)] = [
+        ("Clear Survey", true, 1),
+        ("Clear Timed Walk", true, 2),
+        ("Clear Subject", true, 3)
+    ]
+
+//    @State var saying: String = ""
+    @Environment(\.dismiss) private var dismiss
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 80.0){
+            List {
+                ForEach(clearingTitles, id: \.self.0) { pair in
+                    Button(action: {
+                        lastID = pair.2
+                    }, label: {
+                        Text(pair.0)
+                    })
+                    .disabled(!pair.1)
+                    .padding()
+                }
+            }
+        }
+        .navigationTitle("Clear Data")
+        .toolbar {
+            Button("Done", action: {dismiss()})
+        }
+    }
+}
+
+struct ClearingView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ClearingView()
+                .navigationTitle("Clear")
+        }
+    }
 }
 
 struct SetupView: View {
@@ -38,35 +77,48 @@ struct SetupView: View {
     @FocusState private var controlFocus: ControlFocus?
 
     @AppStorage(AppStorageKeys.reportAsMagnitude.rawValue) var asMagnitude = false
-    @AppStorage(AppStorageKeys.walkInMinutes.rawValue) var duration = 6
+    @AppStorage(AppStorageKeys.walkInMinutes.rawValue)  var duration = 6
     @AppStorage(AppStorageKeys.reportingEmail.rawValue) var email = ""
+    @AppStorage(AppStorageKeys.subjectID.rawValue)      var subjectID = ""
+    @AppStorage(AppStorageKeys.includeWalk.rawValue)    var includeWalk = true
+    @AppStorage(AppStorageKeys.includeSurvey.rawValue)  var includeSurvey = true
+
+    @State private var showingClears = false
 
     var body: some View {
         NavigationView {
-//            VStack {
-                Form {
-                    Section("Walk") {
-                        Stepper("Duration (\(self.duration)):",
-                                value: $duration,
-                                in: AppStorageKeys.dasiWalkRange,
-                                step: 1,
-                                onEditingChanged: { _ in
-                            controlFocus = nil
-                        })
-
-                    }
-                    Section("Reporting") {
-                        Toggle("Report magnitude",
-                               isOn: $asMagnitude)
-                        // FIXME: need a binding for the email
-                        EmailFormView(title: "Email",
-                                      address: $email)
-//                            .border(.blue)
-                    }
+            Form {
+                Section("Walk") {
+                    Toggle("Perform Timed Walk", isOn: $includeWalk)
+                    Stepper("Duration (\(self.duration)):",
+                            value: $duration,
+                            in: AppStorageKeys.dasiWalkRange,
+                            step: 1,
+                            onEditingChanged: { _ in
+                        controlFocus = nil
+                    })
                 }
-//            }
+                Section("DASI Survey") {
+                    Toggle("Perform DASI Survey", isOn: $includeSurvey)
+                }
+
+                Section("Reporting") {
+                    Toggle("Report as magnitude",
+                           isOn: $asMagnitude)
+                    // FIXME: need a binding for the email
+                    EmailFormView(title: "Email",
+                                  address: $email)
+                    //                            .border(.blue)
+                }
+                Section("Collected data") {
+                    NavigationLink("Clear Data", // isActive: $showingClears,
+                                   destination: {
+                        ClearingView()
+                            .navigationBarBackButtonHidden(true)
+                    })
+                }
+            }
             .navigationTitle("Configuration")
-//            .padding()
         }
     }
 }
