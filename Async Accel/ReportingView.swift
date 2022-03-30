34c944acd047878a6a4872103d24b10b77a8e35b
Fritz Anderson <fritza@uchicago.edu>
2022-03-30 15:10:55 -0500

    Forgot to include ReportingView.swift in the previous commit.

---
+++ Async Accel/ReportingView.swift
@@ -0,0 +1,23 @@
+//
+//  ReportingView.swift
+//  Async Accel
+//
+//  Created by Fritz Anderson on 3/30/22.
+//
+
+import SwiftUI
+
+struct ReportingView: View {
+    var body: some View {
+        Text("This space for rent")
+            .navigationTitle("Reporting")
+    }
+}
+
+struct ReportingView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ReportingView()
+            }
+    }
+}
