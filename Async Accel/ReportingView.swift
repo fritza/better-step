02b0405e78afb509e681c8270b5194894c71edb7
Fritz Anderson <fritza@uchicago.edu>
2022-04-04 15:20:57 -0500

    Begin implementaton of ReportingView, particularly calling-out incomplete phases.

--- Async Accel/ReportingView.swift
+++ Async Accel/ReportingView.swift
@@ -8,16 +8,49 @@
 import SwiftUI
 
 struct ReportingView: View {
+    let completionItems: [StatusItem]
+
+    // How are we going to do this, with no actual task?
+    private static func fillCompletionList() -> [StatusItem] {
+        let theOneItem: StatusItem
+        if ContentView.hasCollected {
+            theOneItem = StatusItem(
+                completed: true, title: "Accelerometry",
+                content: "You’ve tried collecting accelerometery.")
+        }
+        else {
+            theOneItem = StatusItem(
+                completed: false, title: "Acelerometry",
+                content: "You haven’t tried accelerometry yet.")
+        }
+        return [theOneItem]
+    }
+
+    var allComplete: Bool {
+        completionItems.allSatisfy(\.completed)
+    }
+
+    init() {
+        completionItems = Self.fillCompletionList()
+    }
+
     var body: some View {
-        Text("This space for rent")
+        NavigationView {
+            VStack {
+                Text("Data collection \(allComplete ? "is now" : "is not yet") complete.")
+                .font(.headline)
+                CompletionDisplayView(items: completionItems)
+                    .frame(height: 120)
+                    .padding()
+            }
             .navigationTitle("Reporting")
+        }
     }
 }
 
 struct ReportingView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
             ReportingView()
-            }
+        .environmentObject(SubjectID.shared)
     }
 }
