a27f9a72ccd709d351a7aa4848e0978f6a4315fc
Fritz Anderson <fritza@uchicago.edu>
2022-04-07 16:34:06 -0500

    Begin instruction-page views, and InstructionElement for specifying their content.

---
+++ Better Step/App Stages/Instructional/WalkInstructionBase.swift
@@ -0,0 +1,24 @@
+//
+//  WalkInstructionBase.swift
+//  Async Accel
+//
+//  Created by Fritz Anderson on 4/7/22.
+//
+
+import SwiftUI
+
+struct WalkInstructionBase: View {
+    @State private var counter: Int = 0
+
+    var body: some View {
+        NavigationView {
+            
+        }
+    }
+}
+
+struct WalkInstructionBase_Previews: PreviewProvider {
+    static var previews: some View {
+        WalkInstructionBase()
+    }
+}
