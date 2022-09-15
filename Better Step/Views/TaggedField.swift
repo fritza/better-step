e3b9cd294fd9aca73f0d912bf198aff8dd9e704f
Fritz Anderson <fritza@uchicago.edu>
2022-09-15 18:31:59 -0500

    New view TaggedField, which has a clear-all, a callback result on commit, etc.
    See comment above showInstructions for a frustrating lag in validity checks.

---
+++ Better Step/Views/TaggedField.swift
@@ -0,0 +1,108 @@
+//
+//  TaggedField.swift
+//  Better Step
+//
+//  Created by Fritz Anderson on 9/15/22.
+//
+
+import SwiftUI
+
+struct TaggedField: View {
+
+    @State var subject: String
+    @State var showComment: Bool = false
+    // ShowInstructions lags the subject string by one.
+    // I think this has to do with _both_ updating the
+    // backing string _and_ rendering conditioned on the
+    // (existing pre-change?) value.
+    @State var showInstructions: Bool = true
+    let closeCallback: (String) -> Void
+
+    var canSubmitText: Bool {
+        return subject.trimmed?.isAlphanumeric ?? false
+    }
+
+//    @Binding var subject: String
+
+    init(subject: String, callback: @escaping ((String) -> Void)) {
+        self.subject = subject
+        self.closeCallback = callback
+    }
+
+    var body: some View {
+        VStack {
+            ZStack(alignment: .trailing) {
+                TextField("IGNORED 1",
+                          text: $subject,
+                          prompt: Text("reporting address"))
+                    .keyboardType(.emailAddress)
+                    .textFieldStyle(.roundedBorder)
+    //                        .focused($amSelected)
+                Button(action: {
+                    subject = ""
+
+                }) {
+                    Image(systemName: "multiply.circle.fill")
+                        .renderingMode(.template)
+                        .foregroundColor(.gray)
+                        .opacity(0.5)
+                }
+                .onChange(of: subject, perform: { newText in
+                    self.showComment = false
+                    self.showInstructions = !canSubmitText
+                })
+                .padding([.trailing], 2)
+            }
+            Spacer()
+            Text("current is \(subject.trimmed ?? "EMPTY")")
+            Spacer()
+
+            Button("Proceed") {
+                if canSubmitText {
+                    showComment = true
+                    closeCallback(subject.trimmed!)
+                    showInstructions = false
+                }
+            }
+            .disabled(!self.canSubmitText)
+            if showComment {
+                Text("Closed with \(subject)")
+                    .foregroundColor(.red)
+            }
+
+            if showInstructions {
+                Spacer()
+                Text("Your ID consists of letters and numbers, no punctuation or spacing.")
+                    .font(.body)
+                    .foregroundColor(.red)
+                    .lineLimit(6)
+//                    .minimumScaleFactor(0.5)
+                    .frame(width: 300, height: 80)
+            }
+
+        }
+        .onSubmit(of: .text) {
+            showComment.toggle()
+        }
+    }
+}
+
+final class HoldsAString: ObservableObject {
+    @State var someSubjectID: String
+    init(subject: String) {
+        someSubjectID = subject
+    }
+}
+
+struct TaggedField_Previews: PreviewProvider {
+    static let holder = HoldsAString(subject: "Thirsday")
+    static var content = ""
+    static var previews: some View {
+        NavigationView {
+            TaggedField(subject: "Erewhon", callback: { result in
+                print("Hello?", result)
+            })
+            .frame(width: 300, height: 48)
+        }
+    }
+}
