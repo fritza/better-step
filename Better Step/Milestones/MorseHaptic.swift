92f5ba593e918e1829c72a0a53be38852816c65f
Fritz Anderson <fritza@uchicago.edu>
2022-09-30 13:18:10 -0500

    Add struct MorseHaptic plus its supporting AHAP config files.
    The hope is for client code simply to call and forget "try MorseHaptic.aaa.play()"
    Rename HapticMilestone.swift to MorseHaptic.swift

---
+++ Better Step/Milestones/MorseHaptic.swift
@@ -0,0 +1,77 @@
+//
+//  MorseHaptic.swift
+//  Better Step
+//
+//  Created by Fritz Anderson on 9/20/22.
+//
+
+import Foundation
+import CoreHaptics
+
+/// Player for a haptic in a named AHAP file `rawValue`, a `String`, is the base name for the corresponding AHAP pattern file.
+///
+/// - note: ATW, `MorseHaptic` relies on `.init(contentsOf:)`, which is iOS 16-only.
+///                 It should be possible to generate the patterns programmatically, but this gets a beta out the door.
+/// - warning: `MorseHaptic` relies on `throws` everywhere _except_ the initailzer, which `RawRepresentable` insists must be a non-throwing failable.
+struct MorseHaptic: RawRepresentable {
+    /// `RawRepresentable` adoption
+    let rawValue        : String
+    /// The haptic pattern derived from the AHAP file
+    var chPattern       : CHHapticPattern?
+    /// The location of the AHAP file
+    let url             : URL
+    /// Singleton haptic engine
+    static var engine   : CHHapticEngine!
+
+    /// Initialize all properties (except `chPattern)` from the base name..
+    init?(rawValue   : String) {
+        do {
+            // RawRepresentable compliance
+            self.rawValue = rawValue
+
+            // URL of the AHAP pattern file
+            guard let aURL = Bundle.main.url(forResource: self.rawValue, withExtension: "ahap") else {
+                // Convert the nil to an Error.
+                // Doesn't matter in this application, but it's good hygeine.
+                let userInfo: [String: Any] = [
+                    NSLocalizedDescriptionKey :
+                        "Could not find the .ahap “\(rawValue)”"
+                ]
+                throw NSError(domain: "KMHaptics", code: 1,
+                              userInfo: userInfo)
+            }
+
+            url = aURL
+            // Create no more than one engine
+            Self.engine = try Self.engine ?? CHHapticEngine()
+            try Self.engine.start()
+        }
+        catch {
+#if DEBUG
+            print(#function, "must return nil:", error.localizedDescription)
+#endif
+            return nil
+        }
+    }
+
+    /// The `CHHapticPattern` derived from the `rawValue` value of `self`.
+    mutating func pattern() throws -> CHHapticPattern {
+        if let chPattern { return chPattern }
+        chPattern = try CHHapticPattern(contentsOf: url)
+        return chPattern!
+    }
+
+    mutating func play() throws {
+        // Remember that the engine will hold on to the
+        // player for the duration. There's no need
+        // to assign it to a variable.
+        let player: CHHapticPatternPlayer =
+        try Self.engine
+            .makePlayer(with: try pattern())
+        try player.start(atTime: CHHapticTimeImmediate)
+    }
+
+    static let aaa = MorseHaptic(rawValue: "AAA")
+    static let nnn = MorseHaptic(rawValue: "NNN")
+}
+
