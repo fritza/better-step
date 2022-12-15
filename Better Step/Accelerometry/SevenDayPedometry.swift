dff38883b7f1e11e382509346fb209235bd36d51
Fritz Anderson <fritza@uchicago.edu>
2022-12-15 13:38:08 -0600

    Add SevenDaysMockup.csv for mocking incoming 7-day reporting
    Add compilation condition "DUMMY_PEDOMETRY"

--- Better Step/Accelerometry/SevenDayPedometry.swift
+++ Better Step/Accelerometry/SevenDayPedometry.swift
@@ -18,7 +18,48 @@ import CoreMotion
 //      (empty) interval, date, count.
 //
 
-final class Pedometry {
+/*
+ Now, wasn't I able to collect thousands of records from elsewhere in Core Motion?
+ */
+
+
+final class Pedometry: ReportingPhase {
+    typealias SuccessValue = [String]
+    var completion: ClosureType
+    init(_ closure: @escaping ClosureType) {
+        completion = closure
+    }
+
+    func proceed() {
+        let mb = Bundle.main
+        guard let sampleURL =
+                mb.url(forResource: "SevenDaysMockup", withExtension: "csv")
+        else {
+            fatalError("Could not find SevenDaysMockup.csv")
+        }
+
+        let fileContent = try! String(contentsOf: sampleURL)
+        let records = fileContent.components(separatedBy: "\n").dropLast()
+        // FIXME: Remove empty string at end.
+        let tagged = records.map { "\(SeriesTag.sevenDayRecord.rawValue),\(SubjectID.id)," + $0 }
+        let result: ResultValue = .success(tagged)
+        completion(result)
+    }
+
+    let calendar = Calendar(identifier: .gregorian)
+    func dayRange() -> [String] {
+        return (0...6).map {
+            days in
+            return calendar.date(byAdding: .day, value: -days, to: Date())!
+        }
+        .map(\.ymd)
+    }
+}
+/*
+#else
+final class Pedometry: ReportingPhase {
+    typealias SuccessValue = CMPedometerData
+
     /// The shared pedometry service, or `nil` if none is available.
     static let cmPedometer: CMPedometer? =
     {
@@ -30,18 +71,38 @@ final class Pedometry {
     }()
 
     static let calendar = Locale.current.calendar
+
+
+    // FIXME: Unresolved problem of n-day history
     static let dayCount = 7
+    // To-wit: Do we submit this set if the previous isn't stale yet? And do we have since-last, or the whole 7 days again.
+    // Also bear in mind that when you ask CM back to "7 days ago" it's subject to a 7-day cutoff (I understand this to be if it's noon today, then the oldest and the current daily records will be truncated by half.
 
 
-    init?() {
+    let completion: ClosureType
+    #warning("")
+
+//    {
+//        // TODO: Why a computed property?
+//
+//        let rightNow = Date()
+//        let differencs = DateComponents(day: Self.dayCount)
+//        let lastMidnight = Self.calendar.startOfDay(for: rightNow)
+//
+//        guard let sevenDaysBefore = Self.calendar.date(
+//            byAdding: .day, value: -7, to: lastMidnight) else {
+//            fatalError("Can't calculate ~7 days before last midnight")
+//        }
+//    }
+
+    init?(_ closure: @escaping ClosureType) {
         guard
             Self.cmPedometer != nil,
             CMPedometer.authorizationStatus() == .authorized
         else { return nil }
+        self.completion = closure
     }
 
-    typealias PedometerResult = Result<CMPedometerData, Error>
-    typealias PedometerCallback = (PedometerResult) -> Void
     func dateSpan(completion: @escaping PedometerCallback) {
         // Return some kind of begin/end interval by calendar date
         // Return it at an interval.
@@ -60,15 +121,24 @@ final class Pedometry {
 
         // SEE BELOW [*1*] for the need to drop partials at one or both ende.
 
-        pedometer.queryPedometerData(from: sevenDaysBefore,
-                                     to: lastMidnight)
+    }
+
+    func proceed() {
+        // TODO: Make this async.
+        pedometer.queryPedometerData(
+            from: sevenDaysBefore,
+            to: lastMidnight)
         { pedData, error in
             // It's escaping. You can't really signal failure.
-            let retval: PedometerResult
+            let retval: ResultValue
             if let error {
                 retval = .failure(error)
             }
             else if let pedData {
+
+
+
+
                 retval = .success(pedData)
             }
             else {
@@ -80,6 +150,9 @@ final class Pedometry {
 
     #warning("Add a phase for the 7-day walk")
 }
+
+#endif
+ */
 
 /*
  [*1*]
