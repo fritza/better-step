3f74233821e13c4bf4404ceaf3d33d1b9d2ffced
Fritz Anderson <fritza@uchicago.edu>
2022-12-16 10:40:54 -0600

    New ApplicationGreetingView for the non-SubjectID (after-first-run) launch of the app.

---
+++ Better Step/Views/ApplicationGreetingView.swift
@@ -0,0 +1,20 @@
+//
+//  ApplicationGreetingView.swift
+//  Better Step
+//
+//  Created by Fritz Anderson on 12/16/22.
+//
+
+import SwiftUI
+
+struct ApplicationGreetingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ApplicationGreetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ApplicationGreetingView()
+    }
+}
