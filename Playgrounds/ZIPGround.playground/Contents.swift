84e5800fa95f5913f9cf76291ae0f491ecd19075
Fritz Anderson <fritza@uchicago.edu>
2022-12-27 12:57:16 -0600

    Turn the project into a workspace, add a playground, and experiment with getting insight into the app code.

---
+++ Playgrounds/ZIPGround.playground/Contents.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+//import Better_Step
+import ZIPFoundation
+
+
+let tag = SeriesTag.dasi
+print(tag)
+
+//let oneTag = SeriesTag.usability
+
+//let bundleURL = Bundle.main.bundleURL
+//print(bundleURL.path)
+//
+////FileManager.default.whatsThere(at: bundleURL)
+//let exist = FileManager.default
+////    .fileExists(atPath: bundleURL.path)
+//    .fileExists(atURL: bundleURL)
+//print(exist)
+
+
+
+//
+//do {
+//    print("Doing the ZA!")
+//}
+//catch {
+//    print("Creating CSVArchiver failed:", error)
+//}
+
+/*
+ Found this candidate (/Users/fritza/Personal-Projects/bstep-isolation/better-step/Better Step/Extensions/FileManager+extensions.swift)
+ */
+
+
+/*
+ Found this candidate (/Users/fritza/Personal-Projects/bstep-isolation/better-step/Playgrounds/ZIPGround.playground/Sources/FileManager+extensions.swift)
+ */
