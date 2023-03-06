92ff6ac7789db89aa829fdffe233ecabbe616fc1
Fritz Anderson <anderson.fritz@gmail.com>
2023-03-06 11:28:19 -0600

    Add ToyInstructions target to reform the pre-walk instruction series.
    BAD FEELING. No way to insert the Volume by hand?

---
+++ ToyInstructions/ToyInstructionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyInstructionsApp.swift
+//  ToyInstructions
+//
+//  Created by Fritz Anderson on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ToyInstructionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
