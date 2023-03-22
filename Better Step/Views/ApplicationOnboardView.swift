88a8c15b7e52ba57fcd7ee1ea095b8302d90cc8e
Fritz Anderson <anderson.fritz@gmail.com>
2023-03-22 13:22:30 -0500

    ApplicationOnboardView: Reespond to SubjectID edits by disabling the Submit button until the text matches the format.

--- Better Step/Views/ApplicationOnboardView.swift
+++ Better Step/Views/ApplicationOnboardView.swift
@@ -22,29 +22,19 @@ struct ApplicationOnboardView: View, ReportingPhase {
         case idField
         case submitButton
     }
-
+    
     typealias SuccessValue = String
     var completion: ClosureType
-
+    
     let item: TaskInterstitialDecodable
-
-    @State      private var submissionRemarks = ""
+    
+    @State      private var workingString = SubjectID.id
     /// Throughout the app, there will be a `@State` `Bool` controlling the visibility of an alert for winding the app back to a virgin state.
-    @State      private var shouldWarnOfReversion = false
     @Binding    private var targetString   : String
     @FocusState private var currentFocus: Focusables?
-
-    var fieldIsEmpty: Bool {
-        guard let trimmedValue = targetString.trimmed else { return true }
-        return trimmedValue.isEmpty // trimmedValue.isEmpty
-    }
-
-    var stringIsValid: Bool {
-        guard let trimmable = targetString.trimmed else { return false }
-        // TODO: Shouldn't we trim it in the field?
-        // Probably not, the text would squirt out from under the user.
-
-        return trimmable.isAlphanumeric
+        
+    private var isEntryAcceptable: Bool {
+        workingString.asValidSubjectID != nil
     }
 
     /// Initialize the view given the content information and a button-action closure
@@ -63,7 +53,7 @@ struct ApplicationOnboardView: View, ReportingPhase {
             else {
                 do {
                     
-                    #warning("loading a 2-card JSON for a 1-card context.")
+#warning("loading a 2-card JSON for a 1-card context.")
                     
                     guard let url = Bundle.main.url(forResource: "onboard-intro", withExtension: "json") else {
                         throw FileStorageErrors.cantFindURL(#function)
@@ -83,59 +73,64 @@ struct ApplicationOnboardView: View, ReportingPhase {
             currentFocus = .idField
             // The JSON title is ignored in favor of whatever presenting ViewBuilder puts into the navigationTitle.
         }
-
+    
     private func propagateSuccess() {
         // Propagate the result string (from onSubmit or Submit button)
         // downstream to OnboardContainerView,
         // which propagates to TopContainerView,
         // which sets SubjectID.id.
-        completion(.success(targetString))
+        completion(.success(workingString))
     }
-
-
+    
     // MARK: - body
     var body: some View {
-            // TODO: Copied directly from InterstitialPageView
-            VStack {
-                #warning("Port GenericContainer view")
-                // ... as much as possible, having the
-                // text field breaks the GCV model.
-                // MARK: Instructional text
-                Text((item.contentAbove ?? "Can't Happen").addControlCharacters)
-                    .font(Rendering.bodyFont)
-                    .minimumScaleFactor(Rendering.textMinScale)
-                Spacer(minLength: 30)
-                // MARK: SF Symbol
-                Image(systemName: item.systemImage ?? "bolt.slash.fill")
-                    .scaledAndTinted()
-                    .frame(height: 200)
-
-                Spacer()
-                Text((item.contentBelow ?? "Can't Happen").addControlCharacters)
-                    .font(Rendering.bodyFont)
-                    .minimumScaleFactor(Rendering.textMinScale)
-                Spacer()
-
-                Divider()
+        // TODO: Copied directly from InterstitialPageView
+        VStack {
+            Text("Welcome").font(.largeTitle)
+            Spacer()
+#warning("Port GenericContainer view")
+            // ... as much as possible, having the
+            // text field breaks the GCV model.
+            // MARK: Instructional text
+            Text((item.contentAbove ?? "Can't Happen").addControlCharacters)
+                .font(Rendering.bodyFont)
+                .minimumScaleFactor(Rendering.textMinScale)
+            Spacer(minLength: 30)
+            // MARK: SF Symbol
+            Image(systemName: item.systemImage ?? "bolt.slash.fill")
+                .scaledAndTinted()
+                .frame(height: 200)
+            
+            Spacer()
+            Text((item.contentBelow ?? "Can't Happen").addControlCharacters)
+                .font(Rendering.bodyFont)
+                .minimumScaleFactor(Rendering.textMinScale)
+            Spacer()
+            
+            Divider()
+            
+            Group {
+                TaggedField(string: $workingString)
+                    .onSubmit {
+                        guard let str = workingString.asValidSubjectID else { return }
+                        targetString = str
+//                        guard !targetString.isEmpty else { return }
+                        propagateSuccess()
+                    }
+                    .font(.title)
+                    .focused($currentFocus,
+                             equals: .idField)
                 
-                TaggedField(string: $targetString)
-                .onSubmit {
-                    guard !targetString.isEmpty else { return }
-                    propagateSuccess()
-                }
-                .focused($currentFocus,
-                         equals: .idField)
-
-
+                Text("Subject IDs consist of a single letter followed by a four-digit number from 1000 to 9999.")
+                    .font(.callout)
+                
                 // MARK: The action button
                 Spacer()
-                Button("Submit", action: propagateSuccess)
-//                {propagate(success: targetString)}
-                .disabled(fieldIsEmpty)
-                .navigationTitle("Welcome")
-                .focused($currentFocus,
-                         equals: .submitButton)
+                Button("Submit") { propagateSuccess() }
+                    .disabled(!isEntryAcceptable)
+                    .focused($currentFocus, equals: .submitButton)
             }
+        }
     }
 }
 
@@ -156,28 +151,26 @@ struct OnboardView_Previews: PreviewProvider {
             fatalError("trying to decode \(error.localizedDescription)")
         }
     }
-
+    
     /// **Used in a preview only.**
     final class Edited: ObservableObject {
         @State var editedText: String = ""
     }
-
+    
     static let edits = Edited()
-
+    
     static var previews: some View {
         NavigationView {
             VStack {
                 ApplicationOnboardView(
                     string: edits.$editedText,
                     info: configuration()!, proceedCallback: { result in
-                    if let newID = try? result.get() {
-                        edits.editedText = newID
-                    }
-                })
+                        if let newID = try? result.get() {
+                            edits.editedText = newID
+                        }
+                    })
                 .frame(width: .infinity)//, height: 300)
                 .padding()
-
-                Text("value is '\(edits.editedText)'")
             }
         }
     }
